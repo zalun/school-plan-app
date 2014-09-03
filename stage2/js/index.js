00af92473f23011113493b613406939b31ddf24b
Piotr Zalewa <piotr@zalewa.info>
2014-09-03 15:16:45 +0200

    Stage 1 is a plain html app (no JS involved) Stage 2 is an app with cards (not ready yet)

---
+++ stage2/js/index.js
@@ -0,0 +1,19 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    receivedEvent: function(id) {
+        console.log('Received Event: ' + id);
+    }
+}
+
+window.load = function() {
+  app.initialize();
+}
