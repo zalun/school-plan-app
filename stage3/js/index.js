a7f6ea8dd5951cdf83b5459232d96e0639c6f640
Piotr Zalewa <piotr@zalewa.info>
2014-09-04 14:01:50 +0200

    working fine on FFOS, fails on Android

--- stage3/js/index.js
+++ stage3/js/index.js
@@ -15,18 +15,15 @@ var app = {
         var slideThreshold = 100;
         app.planGroup = document.getElementById('plan-group');
         app.planGroup.loop = true;
-        app.planGroup.addEventListener('touchstart', function(evt) {
-            var touches = evt.changedTouches;
-            if (touches.length == 1) {
-                startX = touches[0].pageX;
-            }
-        });
-        app.planGroup.addEventListener('touchend', function(evt) {
-            var endX;
-            var moveX;
-            if (startX) {
-                endX = evt.changedTouches[0].pageX;
-                moveX = endX - startX;
+        
+        function touchStart(sX) {
+            startX = sX;
+        }
+
+        function touchEnd(eX) {
+            if (startX) { 
+                var moveX;
+                moveX = eX - startX;
                 if (Math.abs(moveX) > slideThreshold) {
                     if (moveX > 0) {
                         app.nextPlan();
@@ -34,8 +31,23 @@ var app = {
                         app.previousPlan();
                     }
                 }
+                startX = null;
             }
-            startX = null;
+        }
+
+        app.planGroup.addEventListener('touchstart', function(evt) {
+            var touches = evt.changedTouches;
+            if (touches.length == 1) {
+                touchStart(touches[0].pageX);
+            }
+        });
+
+        // app.planGroup.addEventListener('mousedown', function(evt) {
+        //     console.log('mousedown', evt);
+        //     touchStart(evt.clientX);
+        // });
+        app.planGroup.addEventListener('touchend', function(evt) {
+            touchEnd(evt.changedTouches[0].pageX);
         });
     },
     previousPlan: function() {
