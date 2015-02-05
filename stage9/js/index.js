c8809a4aa2258844344b0bb5f74edd81e769b1f5
Piotr Zalewa <piotr@zalewa.info>
2015-02-05 12:08:08 +0100

    removed global variables

--- stage9/js/index.js
+++ stage9/js/index.js
@@ -1,26 +1,13 @@
-var flipbox = document.querySelector('brick-flipbox');
-var plansURL;
-var dayOfWeek;
-var hashtags = [];
-var oldHashtags = [];
-var selectedPlan;
-
-function isConfigured() {
-    var isConfigured = false;
-    if (plansURL && hashtags && hashtags.length > 0) {
-        for (i = 0; i < hashtags.length; i++) {
-            if (hashtags[i]) {
-                isConfigured = true;
-            }
-        }
-    }
-
-    return isConfigured;
-}
-
 var app = {
     plans: [],
     planGroup: null,
+    flipbox: null,
+    hashtags: null,
+    plansURL: null,
+    dayOfWeek: null,
+    oldHashtags: [],
+
+
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -134,7 +121,7 @@ var app = {
             weekDayNumber = i % 7;
             if (plan.week[planDayNumber].length > 0) {
                 th = document.createElement('th');
-                th.appendChild(document.createTextNode(dayOfWeek.value[weekDayNumber]));
+                th.appendChild(document.createTextNode(app.dayOfWeek.value[weekDayNumber]));
                 tr.appendChild(th);
             }
         }
@@ -176,7 +163,7 @@ var app = {
         request.onload = function() {
             if (this.status !== 200) {
                 console.log('DEBUG: Failed to get ``' 
-                            + plansURL + '/plan/' + hashtag, this.status);
+                            + app.plansURL + '/plan/' + hashtag, this.status);
                 window.alert('ERROR: ' + this.responseText);
                 app.removeUI(hashtag);
                 return;
@@ -197,11 +184,11 @@ var app = {
         };
         request.onerror = function(error) {
             console.log('DEBUG: Failed to get ``' 
-                        + plansURL + '/plan/' + hashtag, error);
-            window.alert('ERROR: Failed to get ' + plansURL + '/plan/' + hashtag);
+                        + app.plansURL + '/plan/' + hashtag, error);
+            window.alert('ERROR: Failed to get ' + app.plansURL + '/plan/' + hashtag);
             app.removeUI(hashtag);
         };
-        request.open("get", plansURL + '/plan/' + hashtag, true);
+        request.open("get", app.plansURL + '/plan/' + hashtag, true);
         request.send();
     },
 
@@ -210,34 +197,30 @@ var app = {
      * get list of the plans for given URL (stored in plansURL)
      */
     reloadPlans: function() {
-        if (!isConfigured()) {
+        var i;
+        for (i = 0; i < app.oldHashtags.length; i++) {
+            localStorage.removeItem(app.oldHashtags[i]);
+            app.removeUI(app.oldHashtags[i]);
+        }
+        if (!app.isConfigured()) {
+            // do not try to load plans if not configured
             return;
         }
-        var i;
-        for (i = 0; i < oldHashtags.length; i++) {
-            localStorage.removeItem(oldHashtags[i]);
-            app.removeUI(oldHashtags[i]);
-        }
-        for (i = 0; i < hashtags.length; i++) {
-            oldHashtags[i] = hashtags[i];
+        for (i = 0; i < app.hashtags.length; i++) {
+            app.oldHashtags[i] = app.hashtags[i];
             // get each plan at a time
             // WARNING: order of the plans is a subject of race condition
-            app.loadPlan(hashtags[i]);
+            app.loadPlan(app.hashtags[i]);
         }
-        flipbox.toggle();
+        app.flipbox.toggle();
     },
 
-
-    onDeviceReady: function() {
-        // SETTINGS
-        var i;
+    setup: function() {
+        app.flipbox = document.querySelector('brick-flipbox');
         var settingsButton = document.getElementById('settings-button');
         var settingsOffButton = document.getElementById('settings-off-button');
-        function toggleFlipbox() {
-            flipbox.toggle();
-        }
-        settingsButton.addEventListener('click', toggleFlipbox);
-        settingsOffButton.addEventListener('click', toggleFlipbox);
+        settingsButton.addEventListener('click', app.toggleSettings);
+        settingsOffButton.addEventListener('click', app.toggleSettings);
 
         var reloadButton = document.getElementById('reload-button');
         reloadButton.addEventListener('touchstart', app.reloadPlans, false);
@@ -245,40 +228,37 @@ var app = {
         // server value
         var serverInput = document.getElementById('server-input');
         // check if already set
-        plansURL = localStorage.getItem('plansURL');
-        if (plansURL) {
-            serverInput.value = plansURL;    
+        app.plansURL = localStorage.getItem('plansURL');
+        if (app.plansURL) {
+            serverInput.value = app.plansURL;    
         }
         // setting server from input
         serverInput.addEventListener('blur', function() {
-            plansURL = serverInput.value;
-            localStorage.setItem('plansURL', plansURL);
+            app.plansURL = serverInput.value;
+            localStorage.setItem('plansURL', app.plansURL);
         });
 
         // read hashtags from localStorage
-        hashtags = localStorage.getItem('hashtags');
-        if (!hashtags) hashtags = '[]' ;
+        var hashtagsString = localStorage.getItem('hashtags') || '[]';
         var hashtagInput;
-        if (hashtags) {
-            hashtags = JSON.parse(hashtags);
-            // set hashtags in the settings
-            for (i = 0; i < hashtags.length; i++) {
-                oldHashtags[i] = hashtags[i];
-                hashtagInput = document.getElementById('input-plan-' + (i + 1));
-                if (hashtags[i]) {
-                    hashtagInput.value = hashtags[i];
-                }
+        app.hashtags = JSON.parse(hashtagsString);
+        // set hashtags in the settings
+        for (i = 0; i < app.hashtags.length; i++) {
+            app.oldHashtags[i] = app.hashtags[i];
+            hashtagInput = document.getElementById('input-plan-' + (i + 1));
+            if (app.hashtags[i]) {
+                hashtagInput.value = app.hashtags[i];
             }
-        } 
+        }
         // setting hashtags from input
         function setHashtags(evt) {
             if (evt.target.value) {
-                hashtags[evt.target.hashtagIndex] = evt.target.value;
+                app.hashtags[evt.target.hashtagIndex] = evt.target.value;
             } else {
-                hashtags[evt.target.hashtagIndex] = null;
+                app.hashtags[evt.target.hashtagIndex] = null;
             }
 
-            localStorage.setItem('hashtags', JSON.stringify(hashtags));
+            localStorage.setItem('hashtags', JSON.stringify(app.hashtags));
         }
         // XXX hardcoded maximum number of plans == 3
         for (i = 1; i < 4; i++) {
@@ -287,24 +267,49 @@ var app = {
             hashtagInput.hashtagIndex = i - 1;
         }
         // flip to settings if not configured yet
-        if (!isConfigured()) {
-            flipbox.toggle();
+        if (!app.isConfigured()) {
+            app.flipbox.toggle();
         }
+
+    },
+
+    toggleSettings: function() {
+        app.flipbox.toggle();
+    },
+
+    isConfigured: function() {
+        var isConfigured = false;
+        if (app.plansURL && app.hashtags && app.hashtags.length > 0) {
+            for (i = 0; i < app.hashtags.length; i++) {
+                if (app.hashtags[i]) {
+                    isConfigured = true;
+                }
+            }
+        }
+
+        return isConfigured;
+    },
+
+    onDeviceReady: function() {
+        // SETTINGS
+        var i;
+
+        app.setup();
 
         // load plans from storage or server
         navigator.globalization.getDateNames(function(dayOfWeekResponded){
-            dayOfWeek = dayOfWeekResponded;
-            if (isConfigured()) {
+            app.dayOfWeek = dayOfWeekResponded;
+            if (app.isConfigured()) {
                 // load plans from storage
-                for (var i=0; i < hashtags.length; i++) {
-                    var planString = localStorage.getItem(hashtags[i]);
+                for (var i=0; i < app.hashtags.length; i++) {
+                    var planString = localStorage.getItem(app.hashtags[i]);
                     if (planString === null) {
                         // something went wrong - let's try to load
                         // plans from server
-                        app.loadPlan(hashtags[i]);
+                        app.loadPlan(app.hashtags[i]);
                     } else {
                         var plan = JSON.parse(planString);
-                        app.drawUI(plan, hashtags[i]);
+                        app.drawUI(plan, app.hashtags[i]);
                     }
                 }
             }
